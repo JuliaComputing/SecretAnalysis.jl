314e657dfd04be9f5948c5a69f8f13422e8b9186
Matt Bauman <mbauman@gmail.com>
2020-04-22 20:59:49 -0500

    Add outputs

--- bin/main.jl
+++ bin/main.jl
@@ -1,7 +1,10 @@
-using SecretAnalysis
+using SecretAnalysis, JSON, Printf, Distributed
 
-n = parse(Int, replace(get(ENV, "n", "10_000_000_000"), '_'=>""))
+n = parse(Int64, replace(get(ENV, "n", "10_000_000_000"), '_'=>""))
 
 println("#### Estimating π with $n throws ####")
 
-@time @show estimate_pi(n)
+t = @timed @show estimate_pi(n)
+
+out = Dict(:pi => t[1], :time => @sprintf("%.1fs", t[2]), :workers => nworkers())
+ENV["OUTPUTS"] = JSON.json(out)
