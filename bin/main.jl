c836fe67b59527ae1fc4240b49328ea6cb5e2ca0
jeremie.db <jeremie.db@evovest.com>
2021-12-03 15:30:53 -0500

    refresh

--- bin/main.jl
+++ bin/main.jl
@@ -1,4 +1,4 @@
-using SecretAnalysis, JSON, Printf, Distributed, JSON3
+using SecretAnalysis, JSON3, Printf, Distributed
 
 n = parse(Int64, replace(get(ENV, "n", "10_000_000_000"), '_'=>""))
 
@@ -6,17 +6,10 @@ println("#### Estimating π with $n throws ####")
 
 t = @timed @show estimate_pi(n)
 
-out_1 = Dict(:pi => t[1], :time => @sprintf("%.1fs", t[2]), :workers => nworkers(), :tag => "RESULTS")
-out_2 = Dict(:pi => t[1], :time => @sprintf("%.1fs", t[2]), :workers => nworkers(), :tag => "RESULTS_FILE")
-
-# ENV["OUTPUTS"] = JSON.json(out)
-# ENV["RESULTS"] = JSON.json(out)
-
-
-ENV["RESULTS"] = JSON3.write(out_1)
-
+out = Dict(:pi => t[1], :time => @sprintf("%.1fs", t[2]), :workers => nworkers())
 open("results.json", "w") do io
-    JSON3.pretty(io, JSON3.write(out_2))
+    JSON3.pretty(io, JSON3.write(out))
 end
 
+ENV["RESULTS"] = JSON3.write(out)
 ENV["RESULTS_FILE"] = "results.json"
